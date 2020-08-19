d443e69c24acb1e35887bdbb4b12872a1816aae1
YosafatM <yosafat_martinez21@hotmail.com>
2020-08-19 17:16:41 -0500

    Subida de proyecto

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,15 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  static Future<dynamic> getCoinData(String base, String currency) async {
+    Response response = await get(
+        'https://rest.coinapi.io/v1/exchangerate/$base/$currency?apikey=F6647ABE-F475-4B11-A521-C4AA5A16A8A3');
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+
+import 'coin_data.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,77 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String currency = 'USD';
+  double rateBTC = 0.0;
+  double rateETH = 0.0;
+  double rateLTC = 0.0;
+
+  Widget getAndroidDropbox() {
+    List<DropdownMenuItem<String>> list = [];
+
+    for (String currency in currenciesList) {
+      list.add(
+        DropdownMenuItem<String>(
+          child: Text(currency),
+          value: currency,
+        ),
+      );
+    }
+
+    return DropdownButton<String>(
+      value: currency,
+      items: list,
+      onChanged: (value) {
+        setState(() {
+          currency = value;
+          updateUI();
+        });
+      },
+    );
+  }
+
+  Widget getCupertinoPicker() {
+    List<Text> list = [];
+
+    for (String currency in currenciesList) {
+      list.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      onSelectedItemChanged: (int value) {
+        setState(() {
+          currency = currenciesList[value];
+          updateUI();
+        });
+      },
+      children: list,
+      itemExtent: 32.0,
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    updateUI();
+  }
+
+  void updateUI() async {
+    dynamic jsResponseBTC = await CoinData.getCoinData('BTC', currency);
+    dynamic jsResponseETH = await CoinData.getCoinData('ETH', currency);
+    dynamic jsResponseLTC = await CoinData.getCoinData('LTC', currency);
+
+    if (jsResponseBTC != null &&
+        jsResponseBTC != null &&
+        jsResponseBTC != null) {
+      setState(() {
+        rateBTC = jsResponseBTC['rate'];
+        rateETH = jsResponseETH['rate'];
+        rateLTC = jsResponseLTC['rate'];
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +102,49 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = ${rateBTC.toStringAsFixed(2)} $currency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 0.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 ETH = ${rateETH.toStringAsFixed(2)} $currency',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(18.0, 0.0, 18.0, 0),
+            child: Card(
+              color: Colors.lightBlueAccent,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                child: Text(
+                  '1 LTC = ${rateLTC.toStringAsFixed(2)} $currency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +159,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getCupertinoPicker() : getAndroidDropbox(),
           ),
         ],
       ),
